1f4ba0674f2239921b99f1b4eb6a7abf31e710e4
iortego42 <iortego42madrid@gmail.com>
2024-11-09 17:52:38 +0100

    Ficheros de utilidades

--- Utils/utils.h
+++ Utils/utils.h
@@ -1,6 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #define COL_RESET "\033[0m"
 #define COL_RED "\033[31m"
 #define COL_GREEN "\033[32m"
 #define COL_YELLOW "\033[33m"
 #define COL_BLUE "\033[34m"
 #define COL_BOLD "\033[1m"
+
+void	errorHandler(int sCode, const char *fName, void (*callback)(void));
--- WinSock/server.c
+++ WinSock/server.c
@@ -1,16 +1,14 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
-#include <stdio.h>
-#include "colors.h"
-
-#pragma comment(lib, "ws2_32")
+#include "utils.h"
 
 void	initWinSockApp(WSADATA* wsaData);
-void	errorHandler(int sCode, const char *fName);
 
 // WSA -> Windows Socket Application
 int main(void) {
 	WSADATA wsaData; // WinSockApp Data
+	SOCKET ListenSocket = INVALID_SOCKET; // Socket
+			
 	printf("["COL_BOLD COL_BLUE"*"COL_RESET"] ");
 	printf("Starting Program\n");
 	initWinSockApp(&wsaData);
@@ -23,19 +21,6 @@ void initWinSockApp(WSADATA*	wsaData) {
 	printf("["COL_YELLOW"-"COL_RESET"] ");
 	printf("Initiliazing socket\n");
 	iResult = WSAStartup(MAKEWORD(2,2), wsaData);
-	errorHandler(iResult, __func__);
-}
-
-
-void errorHandler(int sCode, const char *fName) {
-	if (sCode != 0)
-	{
-		printf("["COL_BOLD COL_RED"*"COL_RESET"] ");
-		printf("Bad status code: %d\n", sCode);
-		printf("["COL_RED"!"COL_RESET"] ");
-		printf("Catched at function: "COL_BOLD"%s"COL_RESET, fName);
-		printf("\n");
-		exit(1);
-	}
-	return ;
+	errorHandler(1, __func__, (void (*)(void))hello_world);
+	errorHandler(iResult, __func__, (void (*)(void))WSACleanup);
 }
