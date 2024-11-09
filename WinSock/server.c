0ae5494d07f5449621dbd630323e08580ce77d03
iortego42 <iortego42madrid@gmail.com>
2024-11-09 18:34:55 +0100

    Bind del servidor

--- WinSock/server.c
+++ WinSock/server.c
@@ -1,6 +1,7 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include "utils.h"
+#define PORT "4444"
 
 void	initWinSockApp(WSADATA* wsaData);
 
@@ -8,10 +9,33 @@ void	initWinSockApp(WSADATA* wsaData);
 int main(void) {
 	WSADATA wsaData; // WinSockApp Data
 	SOCKET ListenSocket = INVALID_SOCKET; // Socket
+	int    iResult;
 			
 	printf("["COL_BOLD COL_BLUE"*"COL_RESET"] ");
 	printf("Starting Program\n");
 	initWinSockApp(&wsaData);
+
+	struct addrinfo *addrData =  NULL, *ptr = NULL, hints;
+	/*memset(&hints, 0, sizeof(hints));*/
+	/*ZeroMemory(&hints, sizeof(hints));*/
+	hints = (struct addrinfo){0};
+
+	// Los dos primeros parametros de getaddrinfo son strings del host (node) y puerto (service)
+	// El puerto puede ser especificado como numero o como servicio, "http" es equivalente a "80"
+	iResult = getaddrinfo(NULL, PORT, &hints, &addrData);
+	errorHandler(iResult, __func__, (void (*)(void))WSACleanup);
+	ListenSocket = socket(addrData->ai_family, addrData->ai_socktype, addrData->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET)
+	{
+		// Importante liberar el heap 
+		freeaddrinfo(addrData);
+		errorHandler(1,__func__, (void (*)(void))WSACleanup);
+	}
+	iResult = bind(ListenSocket, addrData->ai_addr, addrData->ai_addrlen);
+	freeaddrinfo(addrData);
+	errorHandler(1, "bind", (void (*)(void))WSACleanup);
+	// Recibir la conexión
+
 	return 0;
 }
 
@@ -21,6 +45,5 @@ void initWinSockApp(WSADATA*	wsaData) {
 	printf("["COL_YELLOW"-"COL_RESET"] ");
 	printf("Initiliazing socket\n");
 	iResult = WSAStartup(MAKEWORD(2,2), wsaData);
-	errorHandler(1, __func__, (void (*)(void))hello_world);
 	errorHandler(iResult, __func__, (void (*)(void))WSACleanup);
 }
