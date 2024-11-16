8e5107da1f7758141bafb5faf36b1b71173cd98e
iortego42 <iortego42madrid@gmail.com>
2024-11-16 13:25:33 +0100

    Cambios en la gestión de errores
    Se ha implementado que el handler de los errores utilice la macro
    __VA_ARGS__ para asi poder hacer callbacks a cualquier tipo de función
    independientemente del número de argumentos que esta reciba.

--- Utils/utils.c
+++ Utils/utils.c
@@ -1,17 +1,2 @@
 #include "utils.h"
 
-
-void errorHandler(int sCode, const char *fName, void (*callback)(void)) {
-	if (sCode != 0)
-	{
-		printf("["COL_BOLD COL_RED"*"COL_RESET"] ");
-		printf("Bad status code: %d\n", sCode);
-		printf("["COL_RED"!"COL_RESET"] ");
-		printf("Catched at function: "COL_BOLD"%s"COL_RESET, fName);
-		printf("\n");
-		if (callback != NULL)
-			callback();
-		exit(1);
-	}
-	return ;
-}
--- Utils/utils.h
+++ Utils/utils.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 #define COL_RESET "\033[0m"
 #define COL_RED "\033[31m"
@@ -7,5 +8,18 @@
 #define COL_YELLOW "\033[33m"
 #define COL_BLUE "\033[34m"
 #define COL_BOLD "\033[1m"
+// Esta macro nos permite gestionar los errores de la llamada a una función.
+// La macro solo se ejecutara correctamente si callback es una función y si se han
+// proporcionado los parámetros que necesita la propia función para ejecutarse
+// correctamente. En caso contrario, el compilador debería de mostrar un fallo.
+#define ERROR_HANDLER(sCode, funcName,callback, ...) if (sCode != 0) {\
+	printf("["COL_BOLD COL_RED"*"COL_RESET"] ");\
+	printf("Bad status sCode: %d\n", sCode);\
+	printf("["COL_RED "!" COL_RESET"] ");\
+	printf("Catched at function: "COL_BOLD"%s"COL_RESET, __func__);\
+	printf("\n");\
+	if (callback != NULL) callback(__VA_ARGS__);\
+	exit(1);\
+	}
 
-void	errorHandler(int sCode, const char *fName, void (*callback)(void));
+void	errorHandler(int sCode, const char *fName, void (*callback)(void), ...);
