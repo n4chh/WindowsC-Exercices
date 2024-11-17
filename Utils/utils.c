dff7a7e71b5a996856033692be2e880c96346584
iortego42 <iortego42madrid@gmail.com>
2024-11-17 17:02:06 +0100

    Nueva forma de log

--- Utils/utils.c
+++ Utils/utils.c
@@ -1,2 +1,16 @@
 #include "utils.h"
 
+
+void printLog(const logLevel l, const char *format, ...) {
+
+	va_list argptr;
+	
+
+	if (l < LLEVEL || l >= LOGLEVELS)	
+		return;	
+	va_start(argptr, format);
+	printf("%s ", LOGPREFIX[l]);
+	vprintf(format, argptr);
+	va_end(argptr);
+	
+}
--- Utils/utils.h
+++ Utils/utils.h
@@ -1,3 +1,5 @@
+#ifndef UTILS_H
+#define UTILS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -7,6 +9,8 @@
 #define COL_GREEN "\033[32m"
 #define COL_YELLOW "\033[33m"
 #define COL_BLUE "\033[34m"
+#define COL_MAGENTA "\033[35m"
+#define COL_CYAN "\033[36m"
 #define COL_BOLD "\033[1m"
 // Esta macro nos permite gestionar los errores de la llamada a una función.
 // La macro solo se ejecutará correctamente si callback es una función y si se han
@@ -14,13 +18,37 @@
 // correctamente. En caso contrario, el compilador debería de mostrar un fallo.
 #define ERROR_HANDLER(sCode, funcName,callback, ...) \
 	if (sCode != 0) {\
-		printf("["COL_BOLD COL_RED"*"COL_RESET"] ");\
-		printf("Bad status sCode: %d\n", sCode);\
-		printf("["COL_RED "!" COL_RESET"] ");\
-		printf("Catched at function: "COL_BOLD"%s"COL_RESET, funcName);\
-		printf("\n");\
+		printLog(ERR,"Bad status sCode: %d\n", sCode);\
+		printLog(WARNING,"Catched at function: "COL_BOLD"%s"COL_RESET"\n", funcName);\
 		if (callback != NULL) callback(__VA_ARGS__);\
 		exit(1);\
 	}
 
-void	errorHandler(int sCode, const char *fName, void (*callback)(void), ...);
+typedef enum{
+	TRACE,
+	DEBUG,
+	INFO,
+	WARNING,
+	SUCCESS,
+	ERR,
+	LOGLEVELS
+} logLevel;
+
+# ifndef LOGLEVEL
+static logLevel LLEVEL = INFO;
+# else
+static logLevel LLEVEL = LOGLEVEL;
+# endif
+
+
+static char *LOGPREFIX[LOGLEVELS] = {
+	[TRACE] = "["COL_CYAN "."COL_RESET"]",
+	[DEBUG] = "["COL_CYAN COL_BOLD"#"COL_RESET"]",
+	[INFO] = "["COL_BLUE COL_BOLD"*"COL_RESET"]",
+	[WARNING] = "["COL_YELLOW COL_BOLD"-"COL_RESET"]",
+	[SUCCESS] = "["COL_GREEN COL_BOLD"+"COL_RESET"]",
+	[ERR] = "["COL_RED COL_BOLD"!"COL_RESET"]",
+};
+
+void printLog(const logLevel l, const char *format, ...);
+#endif
